c679271b2decdbd2a1e2b85f163fe5a1c19e52f5
Joel Christiansen <greenbadge.jc@gmail.com>
2013-11-24 20:21:38 -0600

    Works, no failsafes or error handleing except in login

--- main.py
+++ main.py
@@ -1,16 +1,20 @@
 import praw
+from time import sleep
+from collections import deque
 
 r = praw.Reddit("Quotesbot v1.0 by /u/Thirdegree")
+done = deque(maxlen=200)
 
 def login_():
 	USERNAME = raw_input("Username\n>")
 	PASSWORD = raw_input("Username\n>")
 	r.login(USERNAME, PASSWORD)
+	return USERNAME
 
 trying = True
 while trying:
 	try:
-		login_()
+		USERNAME = login_()
 		trying = False
 	except praw.errors.InvalidUserPass:
 		print "Incorrect Username or Password"
@@ -20,11 +24,17 @@ def check_quotes(post_body):
 	for i in post_body:
 		if i == '"':
 			quotes +=1
-	return quotes%2
+	return quotes
 
 running = True
 while running:
+	print "Next"
 	comments = r.get_comments("thirdegree")
 	for post in comments:
-		if check_quotes(post.body):
+		quote_number = check_quotes(post.body)
+		if (quote_number%2) and (post.author.name!= USERNAME) and (not(post.id in done)):
 			post.reply('"\nYou dropped this.')
+			print "\"*%d"%quote_number
+			sleep(2)
+		done.append(post.id)
+	sleep(10)
