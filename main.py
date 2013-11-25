f36f93fa097faf1871fee1ca9b002b239285537c
Joel Christiansen <greenbadge.jc@gmail.com>
2013-11-25 00:49:09 -0600

    added comments, was feeling bad

--- main.py
+++ main.py
@@ -12,14 +12,14 @@ def login_():
 	return USERNAME
 
 trying = True
-while trying:
+while trying: # to login
 	try:
-		USERNAME = login_()
+		USERNAME = login_() # There's probably a more "right" way to do this, but I don't know it
 		trying = False
 	except praw.errors.InvalidUserPass:
 		print "Incorrect Username or Password"
 
-def check_quotes(post_body):
+def check_quotes(post_body): # counts the number of quotes in the string
 	quotes = 0
 	for i in post_body:
 		if i == '"':
@@ -28,13 +28,14 @@ def check_quotes(post_body):
 
 running = True
 while running:
-	print "Next"
+	print "Next" #to show it's working
 	comments = r.get_comments("thirdegree")
 	for post in comments:
 		quote_number = check_quotes(post.body)
+		#not even number of quotes, not replying to itself, not already done
 		if (quote_number%2) and (post.author.name!= USERNAME) and (not(post.id in done)):
 			post.reply('"\nYou dropped this.')
-			print "\"*%d"%quote_number
+			print "\"*%d"%quote_number # more to show it's working... I get nervous
 			sleep(2)
 		done.append(post.id)
 	sleep(10)
